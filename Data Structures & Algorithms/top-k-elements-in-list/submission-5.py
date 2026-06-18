7935830dd0dbdcd6f5d7728c2359dcbb4611f00d
Balkis Karoui <122306659+bekitos101@users.noreply.github.com>
2026-06-18 20:04:41 +0200

    Add: top-k-elements-in-list - submission-5

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-5.py
@@ -0,0 +1,34 @@
+import heapq
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #Brute force solution:
+        #traverse the array, for each distinct element we get the count
+        #store these in a frequency map 
+        # return the elements with the K top frequent elements from the frequency map 
+
+        freq_count={}
+
+        for num in nums:
+            freq_count[num]=freq_count.get(num,0)+1
+        
+        #[1:1,2:2,3:3] k=2
+
+        freq_sorted = sorted(freq_count.items(), key=lambda item: item[1],reverse=True)
+
+        print(freq_sorted)
+        result=[]
+
+        count=0
+
+        for item in freq_sorted:
+            result.append(item[0])
+            count+=1
+            if count==k:
+                break
+        return result 
+        
+
+
+
+
+       
